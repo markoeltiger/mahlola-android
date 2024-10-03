1b633539504075c4b2d69e4ae48e4f908090ceeb
Mark Saif <43094629+markoeltiger@users.noreply.github.com>
2024-10-03 11:27:43 +0300

    Init Pager

--- app/src/main/java/com/mark/mahlola/MainActivity.kt
+++ app/src/main/java/com/mark/mahlola/MainActivity.kt
@@ -2,13 +2,14 @@ package com.mark.mahlola
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
 import androidx.compose.animation.Crossfade
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import com.mark.mahlola.features.onboarding.OnBoardingScreen
 
@@ -18,8 +19,9 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.mark.mahlola.core.theme.BACKGROUND_COLOR
 import com.mark.mahlola.features.root.domain.AuthState
-import com.mark.mahlola.ui.theme.MahlolaTheme
+import com.mark.mahlola.core.theme.MahlolaTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -28,7 +30,14 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        enableEdgeToEdge( statusBarStyle = SystemBarStyle.auto(
+            BACKGROUND_COLOR.value.toInt(),
+            BACKGROUND_COLOR.value.toInt()
+        ),
+            navigationBarStyle = SystemBarStyle.auto(
+                BACKGROUND_COLOR.value.toInt(),
+                BACKGROUND_COLOR.value.toInt()
+            ))
         installSplashScreen().setKeepOnScreenCondition {
             viewModel.authState.value == AuthState.UNKNOWN
         }
--- app/src/main/java/com/mark/mahlola/features/onboarding/OnBoardingScreen.kt
+++ app/src/main/java/com/mark/mahlola/features/onboarding/OnBoardingScreen.kt
@@ -1,28 +1,118 @@
 package com.mark.mahlola.features.onboarding
 
+import android.annotation.SuppressLint
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.paint
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.mark.mahlola.R
 import com.mark.mahlola.core.base.collectEvents
 import com.mark.mahlola.core.base.collectState
+import com.mark.mahlola.core.theme.appBoldFontFamily
+import com.mark.mahlola.core.theme.appReguralFontFamily
+import com.mark.mahlola.core.theme.gradientEndColor
+import com.mark.mahlola.core.theme.gradientMiddleColor
+import com.mark.mahlola.core.theme.gradientStartColor
 import com.mark.mahlola.core.ui.ThemedPreview
 
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
-fun OnBoardingScreen(viewModel: OnboardingViewModel, goToAuth:() -> Unit) {
+fun OnBoardingScreen(viewModel: OnboardingViewModel, goToAuth: () -> Unit) {
     val state by viewModel.collectState()
     viewModel.collectEvents {
         when (it) {
             OnboardingEvent.GotoAuth -> goToAuth()
         }
     }
+    Scaffold {
+        Column(
+            modifier = Modifier
+                .padding(it)
+                .fillMaxSize()
+                .background(colorResource(id = R.color.background_color))
+                .paint(painterResource(id = R.drawable.ic_onboarding_background)),
+
+
+        ) {
+            Pager(state = state)
+
+        }
+
+    }
+}
+@OptIn(ExperimentalFoundationApi::class)
+@Composable
+fun ColumnScope.Pager(state: OnboardingState) {
+val pagerState = rememberPagerState(pageCount={state.uiState.pages.size})
+    LaunchedEffect(key1 = state.uiState.currentStep) {
+        pagerState.animateScrollToPage(state.uiState.currentStep)
+    }
+    HorizontalPager(state = pagerState,
+        modifier= Modifier
+            .fillMaxSize()
+            .weight(1f), userScrollEnabled =true
+    ) {page->
+    val pageUiState=state.uiState.pages[page]
+Column (        horizontalAlignment = Alignment.CenterHorizontally,
+
+verticalArrangement = Arrangement.Top ){
     Text(
-        text = "Hello onBoarding!",
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(top = dimensionResource(id = R.dimen.section_title_margin_top)),
+        textAlign = TextAlign.Center,
+        text = pageUiState.title,
+         fontFamily = appBoldFontFamily,
+        style = TextStyle(brush = Brush.linearGradient(colors = listOf(gradientStartColor, gradientMiddleColor,gradientEndColor)), fontSize = 40.sp)
 
     )
+    Text(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding( dimensionResource(id = R.dimen.vertical_screen_margin)),
+        textAlign = TextAlign.Center,
+        fontFamily = appReguralFontFamily,
+        text = pageUiState.description,
+        style = MaterialTheme.typography.bodyMedium
+    )
+
+    Image(
+        modifier = Modifier.size(400.dp), painter = painterResource(id = pageUiState.image),
+        contentDescription = "onboard image"
+    )
+
 }
+    }
+}
+
 @Preview(device = Devices.PIXEL_4_XL)
 @Composable
 private fun Preview() {
